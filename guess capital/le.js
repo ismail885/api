b137e1954dbe06dffe65125807349b5049babca0
ismail885 <ismail.abouzaid@gmail.com>
2024-03-11 21:11:06 +0100

    second commit

--- guess capital/le.js
+++ guess capital/le.js
@@ -14,18 +14,71 @@
 
 // les pays sur lesquels on est questionnée doivent etre généré aléatoirement
 
-const apiUrl = "https://countryapi.io/api/v1/countries/";
-let countries = [];
-let currentQuestion = 0;
-let score = 0;
-
-async function fetchRandomCountry() {
-    try {
-        const response = await fetch(apiUrl + "random");
-        const data = await response.json();
-        return data.country;
-    } catch (error) {
-        console.error(e);
-    }
+c// TXEEg6Myo5I3jwGey72KePUcQGEUyn8JFyhTstrw
+const container = document.querySelector('.container')
+const pays = document.querySelector('.country')
+const img = document.querySelector('img')
+const input = document.querySelector('input')
+const rollBtn = document.querySelector('.rollBtn')
+const answerBtn = document.querySelector('.answerBtn')
+const answer = document.querySelector('.answer')
+let score = 0
+ 
+const key = 'e6XRsAO2xOhGrWBV1uWzqtVPnCJI1DQYzwVin4q9'
+ 
+function getRandomCountry() {
+    axios.get(`https://countryapi.io/api/all?apikey=${key}`)
+        .then(response => {
+            const countries = response.data;
+            const tableau = Object.keys(countries).map(key => countries[key])  
+   
+            const randomCountry = Math.floor(Math.random() * tableau.length);
+            const arrayCountries = tableau[randomCountry]
+            const countryName = arrayCountries.name
+            const countryFlag = arrayCountries.flag.medium
+            const capital = arrayCountries.capital
+            answer.textContent = ""
+ 
+           
+            console.log(capital)
+            console.log(compareResult(capital))
+ 
+ 
+ 
+   
+            pays.textContent = countryName
+            img.src = countryFlag    
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des pays:", error);
+        });
 }
-
+ 
+rollBtn.addEventListener('click', getRandomCountry);
+ 
+document.addEventListener('DOMContentLoaded', getRandomCountry)
+ 
+ 
+function compareResult(capital) {
+ 
+    answerBtn.addEventListener('click', () => {
+        console.log(score)
+ 
+        if(input.value === capital){
+            answer.textContent = "Bonne Réponse"
+            score = score +1
+           
+        } else {
+            console.log('non')
+            answer.textContent = "Mauvaise Réponse"
+        }
+    })
+}
+ 
+window.addEventListener('keypress', (e) => {
+    if(e.key === "Enter"){
+ 
+        answerBtn.click()
+    }
+})
+ 
